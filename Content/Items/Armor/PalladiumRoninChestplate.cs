f4ed24597265941cc5d0b8939b7a7da4b42032a0
Melyguy <101142491+Melyguy@users.noreply.github.com>
2025-05-19 12:20:44 +0200

    new armors

--- Content/Items/Armor/PalladiumRoninChestplate.cs
+++ Content/Items/Armor/PalladiumRoninChestplate.cs
@@ -6,11 +6,11 @@ using Terraria.ModLoader;
 namespace SpiritcallerRoninMod.Content.Items.Armor
 {
 	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
-	// Providing the EquipType.Legs value here will result in TML expecting a X_Legs.png file to be placed next to the item's main texture.
-	[AutoloadEquip(EquipType.Legs)]
-	public class HellstoneRoninLeggings : ModItem
+	// Providing the EquipType.Body value here will result in TML expecting a X_Body.png file to be placed next to the item's main texture.
+	[AutoloadEquip(EquipType.Body)]
+	public class PalladiumRoninChestplate : ModItem
 	{
-		public static readonly float RoninDamageIncrease = 1.03f;
+		public static readonly float RoninDamageIncrease = 15f;
 
 		public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(RoninDamageIncrease);
 
@@ -19,18 +19,19 @@ namespace SpiritcallerRoninMod.Content.Items.Armor
 			Item.height = 18; // Height of the item
 			Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
 			Item.rare = ItemRarityID.Green; // The rarity of the item
-			Item.defense = 10; // The amount of defense the item will give when equipped
+			Item.defense = 15; // The amount of defense the item will give when equipped
 		}
 
 		public override void UpdateEquip(Player player) {
-			player.GetModPlayer<GlobalPlayer>().RoninDamage += RoninDamageIncrease;
+			
+			player.GetModPlayer<GlobalPlayer>().RoninDamage += RoninDamageIncrease / 100f;// Increase how many minions the player can have by one
 		}
 
 		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
-		public override void AddRecipes() {
+public override void AddRecipes() {
 		Recipe recipe = CreateRecipe();
-			recipe.AddIngredient(ItemID.AshWood, 20);
-			recipe.AddIngredient(ItemID.HellstoneBar, 20);
+			recipe.AddIngredient(ItemID.Wood, 15);
+			recipe.AddIngredient(ItemID.CobaltBar, 30);
 			recipe.AddTile(TileID.Anvils);
 			recipe.Register();
 
